5a0cd4493ce60004f48035736190620b010fa167
Zubin Shah <www.zubinshah1886@gmail.com>
2022-04-23 15:03:52 +0530

    enhanced the readability

--- assembler.py
+++ assembler.py
@@ -1,7 +1,3 @@
-import opcode
-from tracemalloc import start
-
-
 OPTAB = {'ADD': '18', 'COMP': '28', 'DIV': '24', 'J': '30',
          'JEQ':'30', 'JGT': '34', 'JLT': '38', 'JSUB': '48', 
          'LDA': '00', 'LDCH': '50', 'LDL': '08', 'LDX': '04',
@@ -12,44 +8,49 @@ OPTAB = {'ADD': '18', 'COMP': '28', 'DIV': '24', 'J': '30',
 with open('input.txt', 'r') as file:
     data = file.readlines()
 
-loc = data[0].split()[-1]
-starting_address = loc
+if data[0].split()[-2] == "START":
+    starting_address = data[0].split()[-1]
+    LOCCTR = starting_address
+else:
+    LOCCTR = "".zfill(4)
+    
+
 program_name = data[0].split()[0]
 while len(program_name)<6:
     program_name += " "
-print(loc)
 objCode = {}
 labels = {}
 backRef = {}
 obj_prog = []
 program_length = ""
+obj_prog.append("H^" + program_name + "^" + starting_address)
 for line in data:
     current = line.split()
     if current[0]=='.':
         continue
     if len(current)==3:
-        labels[current[0]] = loc
+        labels[current[0]] = LOCCTR
 
     if len(current)==1: # handling edge case for RSUB
-        objCode[loc] = OPTAB[current[0]] + "0000"
-        loc = str(hex(int(loc,16) + 3)[2:]).upper()
+        objCode[LOCCTR] = OPTAB[current[0]] + "0000"
+        LOCCTR = str(hex(int(LOCCTR,16) + 3)[2:]).upper()
     elif current[-2] == "RESB":
-        loc = str(hex(int(loc,16) + int(current[2]))[2:])
+        LOCCTR = str(hex(int(LOCCTR,16) + int(current[2]))[2:])
     elif current[-2] == "RESW":
-        loc = str(hex(int(loc,16) + 3*int(current[2]))[2:])
+        LOCCTR = str(hex(int(LOCCTR,16) + 3*int(current[2]))[2:])
     elif current[-2] == "BYTE":
         if current[-1][0] == 'C':
-            objCode[loc] =  ''.join(map(lambda c : str(hex(ord(c))[2:]).upper(), current[-1][2:-1]))
-            loc = str(hex(int(loc,16)+ 3)[2:])
+            objCode[LOCCTR] =  ''.join(map(lambda c : str(hex(ord(c))[2:]).upper(), current[-1][2:-1]))
+            LOCCTR = str(hex(int(LOCCTR,16)+ 3)[2:])
         elif current[-1][0] == 'X':
-            objCode[loc] = current[-1][2:-1]
-            loc = str(hex(int(loc,16)+ 1)[2:])
+            objCode[LOCCTR] = current[-1][2:-1]
+            LOCCTR = str(hex(int(LOCCTR,16)+ 1)[2:])
     elif current[-2]=="START" or current[-2]=="END":
         if current[-2] == "END":
-            program_length = str(hex(int(loc,16) - int(starting_address,16))[2:].upper())
+            program_length = str(hex(int(LOCCTR,16) - int(starting_address,16))[2:].upper())
     elif current[-2]=="WORD":
-        objCode[loc] = hex(int(current[-1]))[2:].zfill(6)
-        loc = str(hex(int(loc,16) + 3)[2:])
+        objCode[LOCCTR] = hex(int(current[-1]))[2:].zfill(6)  #adds leading zeroes 
+        LOCCTR = str(hex(int(LOCCTR,16) + 3)[2:])
     else:
         operand = ""
         if ",X" in current[-1]:
@@ -58,20 +59,20 @@ for line in data:
             operand = current[-1]
 
         if operand in labels.keys():
-            objCode[loc] = OPTAB[current[-2]] + labels[operand]
+            objCode[LOCCTR] = OPTAB[current[-2]] + labels[operand]
         else:
             if operand in backRef.keys():
-                backRef[operand].append(hex(int(loc,16)+1)[2:].upper())
+                backRef[operand].append(hex(int(LOCCTR,16)+1)[2:].upper())
             else:
-                backRef[operand] = [hex(int(loc,16)+1)[2:].upper()]
-            objCode[loc] = OPTAB[current[-2]] + "0000"
+                backRef[operand] = [hex(int(LOCCTR,16)+1)[2:].upper()]
+            objCode[LOCCTR] = OPTAB[current[-2]] + "0000"
 
         if ',X' in current[-1]:
-            objCode[loc]= objCode[loc][:2] + str(int(objCode[loc][2]) | 8) + objCode[loc][3:]
+            objCode[LOCCTR]= objCode[LOCCTR][:2] + str(int(objCode[LOCCTR][2]) | 8) + objCode[LOCCTR][3:]
         else:
-            objCode[loc]= objCode[loc][:2] + str(int(objCode[loc][2]) & 7) + objCode[loc][3:]
+            objCode[LOCCTR]= objCode[LOCCTR][:2] + str(int(objCode[LOCCTR][2]) & 7) + objCode[LOCCTR][3:]
             
-        loc = str(hex(int(loc,16) + 3)[2:])
+        LOCCTR = str(hex(int(LOCCTR,16) + 3)[2:])
     
 
 for key, value in labels.items():
